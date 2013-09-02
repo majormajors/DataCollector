9f95a40e67ce44b93ffec5de7555b510ac681b99
Matt Mayers <matt@mattmayers.com>
2013-09-01 19:44:52 -0700

    The app is collecting data with a service

---
+++ src/com/mattmayers/android/datacollector/DataCollectionService.java
@@ -0,0 +1,176 @@
+package com.mattmayers.android.datacollector;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import java.util.*;
+
+public class DataCollectionService extends Service implements SensorEventListener {
+	private static final String TAG = DataCollectionService.class.getName();
+	private static final int NOTIFICATION_ID = 1000;
+
+	public static final String EXTRA_SENSOR_SWITCH = "com.mattmayers.android.datacollector.EXTRA_SENSOR_SWITCH";
+	public static final String ACTION_SERVICE_STARTED = "com.mattmayers.android.datacollector.ACTION_SERVICE_STARTED";
+	public static final String ACTION_SERVICE_STOPPED = "com.mattmayers.android.datacollector.ACTION_SERVICE_STOPPED";
+
+	public static boolean isRunning = false;
+
+	private final DataCollectionBinder mBinder = new DataCollectionBinder();
+	private NotificationManager mNotifcationManager;
+	private SensorManager mSensorManager;
+
+	private Sensor mPressure;
+	private Sensor mHumidity;
+	private Sensor mAccelerometer;
+	private Sensor mMagnetometer;
+	private Sensor mGyroscope;
+	private Sensor mTemperature;
+
+	private long mStartMillis;
+	private long mEndMillis;
+	private Date mStartTime;
+	private Date mEndTime;
+
+	private SortedMap<Long, float[]> mPressureValues = new TreeMap<Long, float[]>();
+	private SortedMap<Long, float[]> mHumidityValues = new TreeMap<Long, float[]>();
+	private SortedMap<Long, float[]> mAccelerometerValues = new TreeMap<Long, float[]>();
+	private SortedMap<Long, float[]> mMagnetometerValues = new TreeMap<Long, float[]>();
+	private SortedMap<Long, float[]> mGyroscopeValues = new TreeMap<Long, float[]>();
+	private SortedMap<Long, float[]> mTemperatureValues = new TreeMap<Long, float[]>();
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		mNotifcationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+		mPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+		mHumidity = mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+		mTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		if (intent == null || intent.getBooleanExtra(EXTRA_SENSOR_SWITCH, true))
+			startCollecting();
+		else
+			stopCollecting();
+		return START_STICKY;
+	}
+
+	private void dumpSensorData() {
+		mPressureValues.clear();
+		mAccelerometerValues.clear();
+		mHumidityValues.clear();
+		mMagnetometerValues.clear();
+		mGyroscopeValues.clear();
+		mTemperatureValues.clear();
+	}
+
+	private void startCollecting() {
+		mStartMillis = System.currentTimeMillis();
+		mStartTime = Calendar.getInstance().getTime();
+
+		if (mPressure != null)
+			mSensorManager.registerListener(this, mPressure, SensorManager.SENSOR_DELAY_NORMAL);
+		if (mHumidity != null)
+			mSensorManager.registerListener(this, mHumidity, SensorManager.SENSOR_DELAY_NORMAL);
+		if (mAccelerometer != null)
+			mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+		if (mMagnetometer != null)
+			mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+		if (mGyroscope != null)
+			mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+		if (mTemperature != null)
+			mSensorManager.registerListener(this, mTemperature, SensorManager.SENSOR_DELAY_NORMAL);
+
+		synchronized (this) {
+			DataCollectionService.isRunning = true;
+		}
+
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+				.setSmallIcon(android.R.drawable.ic_menu_info_details)
+				.setContentTitle(getString(R.string.app_name))
+				.setContentText(getString(R.string.running))
+				.setOngoing(true);
+		PendingIntent pendingIntent = PendingIntent.getActivity(
+				this, 0, new Intent(this, MainActivity.class), 0);
+		builder.setContentIntent(pendingIntent);
+		mNotifcationManager.notify(NOTIFICATION_ID, builder.build());
+
+		sendBroadcast(new Intent(ACTION_SERVICE_STARTED));
+	}
+
+	private void stopCollecting() {
+		mEndMillis = System.currentTimeMillis();
+		mEndTime = Calendar.getInstance().getTime();
+		mSensorManager.unregisterListener(this);
+		dumpSensorData();
+
+		synchronized (this) {
+			DataCollectionService.isRunning = false;
+		}
+
+		mNotifcationManager.cancel(NOTIFICATION_ID);
+
+		sendBroadcast(new Intent(ACTION_SERVICE_STOPPED));
+		stopSelf();
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		long millis = System.currentTimeMillis();
+
+		switch (event.sensor.getType()) {
+			case Sensor.TYPE_PRESSURE:
+				mPressureValues.put(millis, event.values);
+				break;
+			case Sensor.TYPE_ACCELEROMETER:
+				mAccelerometerValues.put(millis, event.values);
+				break;
+			case Sensor.TYPE_RELATIVE_HUMIDITY:
+				mHumidityValues.put(millis, event.values);
+				break;
+			case Sensor.TYPE_MAGNETIC_FIELD:
+				mMagnetometerValues.put(millis, event.values);
+				break;
+			case Sensor.TYPE_GYROSCOPE:
+				mGyroscopeValues.put(millis, event.values);
+				break;
+			case Sensor.TYPE_AMBIENT_TEMPERATURE:
+				mTemperatureValues.put(millis, event.values);
+				break;
+		}
+
+		Log.d(TAG, String.format("%s: %s", event.sensor.getName(), Arrays.toString(event.values)));
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	}
+
+	private class DataCollectionBinder extends Binder {
+		DataCollectionService getService() {
+			return DataCollectionService.this;
+		}
+	}
+}
---
+++ src/com/mattmayers/android/datacollector/MainActivity.java
@@ -0,0 +1,100 @@
+package com.mattmayers.android.datacollector;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	private Button mStart;
+	private Button mStop;
+	private TextView mStatus;
+
+	private long mStartTime = 0L;
+
+	private BroadcastReceiver mServiceStartedReceiver;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		mServiceStartedReceiver = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				updateUI();
+			}
+		};
+
+		mStatus = (TextView) findViewById(R.id.status);
+		mStart = (Button) findViewById(R.id.start);
+		mStop = (Button) findViewById(R.id.stop);
+
+		mStart.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startCollection();
+			}
+		});
+
+		mStop.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				stopCollection();
+			}
+		});
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		updateUI();
+		registerReceiver(mServiceStartedReceiver,
+				new IntentFilter(DataCollectionService.ACTION_SERVICE_STARTED));
+		registerReceiver(mServiceStartedReceiver,
+				new IntentFilter(DataCollectionService.ACTION_SERVICE_STOPPED));
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		unregisterReceiver(mServiceStartedReceiver);
+	}
+
+	private void startCollection() {
+		Intent intent = new Intent(this, DataCollectionService.class);
+		intent.putExtra(DataCollectionService.EXTRA_SENSOR_SWITCH, true);
+		startService(intent);
+
+		mStartTime = System.currentTimeMillis();
+	}
+
+	private void stopCollection() {
+		Intent intent = new Intent(this, DataCollectionService.class);
+		intent.putExtra(DataCollectionService.EXTRA_SENSOR_SWITCH, false);
+		startService(intent);
+
+		mStartTime = 0L;
+	}
+
+	public void updateUI() {
+		if (DataCollectionService.isRunning) {
+			mStart.setEnabled(false);
+			mStop.setEnabled(true);
+			mStatus.setText(R.string.running);
+			mStatus.setTextColor(Color.GREEN);
+		} else {
+			mStart.setEnabled(true);
+			mStop.setEnabled(false);
+			mStatus.setText(R.string.stopped);
+			mStatus.setTextColor(Color.RED);
+		}
+	}
+}
